49282332a5de02d4158e1bef32c2c50ceeb9e41e
Deepak Wayzode <dwayzode@gmail.com>
2025-04-11 15:34:31 +0530

    well structure whole UI and logic

--- product-listing-page-dynamic/src/components/Navbar.jsx
+++ product-listing-page-dynamic/src/components/Navbar.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import frame from "../assets/images/Frame 34.png"
 import logo from "../assets/images/Logo.png"
 import icons from "../assets/images/Icons.png"
+import searchIcon from "../assets/images/search-normal.png"
+import favIcon from "../assets/images/heart.png"
+import cartIcon from "../assets/images/cart.png"
+import accountIcon from "../assets/images/profile.png"
+import languageIcon from "../assets/images/language-icon.png"
 
 const Navbar = () => {
   return (
@@ -17,8 +22,12 @@ const Navbar = () => {
             <img src={logo} alt="App Logo" />
           </div>
           <div className="Logo_text">LOGO</div>
-          <div className="Feature_Icons">
-            <img src={icons} alt="Feature icons" />
+          <div className="Feature_Icons_Container">
+            <div className='feature-icon'><img src={searchIcon} alt="Search-icon" /></div>
+            <div className='feature-icon'><img src={favIcon} alt="fav-icon" /></div>
+            <div className='feature-icon'><img src={cartIcon} alt="cart-icon" /></div>
+            <div className='feature-icon'><img src={accountIcon} alt="account-icon" /></div>
+            <div className='feature-icon-language'><img src={languageIcon} alt="language-icon" /></div>
           </div>
         </div>
         <div className="Header_Options">
@@ -32,6 +41,9 @@ const Navbar = () => {
             </ul>
           </div>
         </div>
+
+
+
       </header>
     </nav>
   );
